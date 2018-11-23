f58ad3768b9531019963d42a36d20de03d3b5c17
Fuad Osman <1fuadosman@gmail.com>
2018-11-23 23:30:53 +0000

    refactor random email

--- src/main/java/com/wowcher/framework/utilities/EmailGeneratorUtil.java
+++ src/main/java/com/wowcher/framework/utilities/EmailGeneratorUtil.java
@@ -3,8 +3,8 @@ package com.wowcher.framework.utilities;
 public class EmailGeneratorUtil {
 
     public static String generateEmail() {
-        int r = (int)(Math.random() * 999999990);
-        String randomEmail = "wowcherautomation+" + r + "@gmail.com";
+        int randomNumber = (int)(Math.random() * 999999999);
+        String randomEmail = "wowcherautomation+" + randomNumber + "@gmail.com";
         System.out.println("*** Email generated is: " + randomEmail + " ***");
         return randomEmail;
     }
