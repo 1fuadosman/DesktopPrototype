4e6b885443918d301dd68e94f7985ae141030f9c
1fuadosman <1fuadosman@gmail.com>
2018-11-27 14:49:31 +0000

    added waits

--- src/main/java/com/prototype/framework/base/DriverContext.java
+++ src/main/java/com/prototype/framework/base/DriverContext.java
@@ -1,6 +1,12 @@
 package com.prototype.framework.base;
 
+import com.prototype.framework.config.Settings;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class DriverContext {
 
@@ -11,4 +17,31 @@ public class DriverContext {
     public static void setDriver(WebDriver driver) {
         Driver = driver;
     }
+
+    public static void waitForPageToLoad() {
+        WebDriverWait wait = new WebDriverWait(Driver, 30);
+        JavascriptExecutor jsExecutor = (JavascriptExecutor)Driver;
+
+        ExpectedCondition<Boolean> jsLoad = webDriver -> ((JavascriptExecutor)Driver)
+                .executeScript("return document.readyState").toString().equals("complete");
+
+        //Get JS ready
+        Boolean jsReady = jsExecutor.executeScript("return document.readyState").toString().equals("complete");
+
+        if(!jsReady)
+            wait.until(jsLoad);
+        else
+            Settings.Logs.Write("Page is ready!");
+    }
+
+    public static void waitForElementVisible(final WebElement elementFindBy){
+        WebDriverWait wait = new WebDriverWait(Driver, 30);
+        wait.until(ExpectedConditions.visibilityOf(elementFindBy));
+    }
+
+    public static void waitForElementClickable(final WebElement elementFindBy){
+        WebDriverWait wait = new WebDriverWait(Driver, 30);
+        wait.until(ExpectedConditions.elementToBeClickable(elementFindBy));
+    }
+
 }
